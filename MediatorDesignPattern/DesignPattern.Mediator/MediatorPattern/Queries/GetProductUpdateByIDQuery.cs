3275c97c51f1bfa60038c4c5c6c28e665ac9852b
Ali Kurtulmus <mkurtulmus.eng@gmail.com>
2023-06-03 17:28:22 +0300

    Add project files.

---
+++ MediatorDesignPattern/DesignPattern.Mediator/MediatorPattern/Queries/GetProductUpdateByIDQuery.cs
@@ -0,0 +1,15 @@
+﻿using DesignPattern.Mediator.MediatorPattern.Results;
+using MediatR;
+
+namespace DesignPattern.Mediator.MediatorPattern.Queries
+{
+    public class GetProductUpdateByIDQuery:IRequest<UpdateProductByIDQueryResult>
+    {
+        public GetProductUpdateByIDQuery(int productID)
+        {
+            ProductID = productID;
+        }
+
+        public int ProductID { get; set; }
+    }
+}
---
+++ MediatorDesignPattern/DesignPattern.Mediator/Migrations/20230602100730_mig1.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DesignPattern.Mediator.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ProductCategory",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ProductCategory",
+                table: "Products",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+    }
+}
